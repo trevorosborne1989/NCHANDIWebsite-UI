9c6fe59611af46c9f945f4ed9a7948a7caa17438
Trevor Osborne <trevor.osborne@srs.gov>
2023-08-27 00:12:26 -0700

    added Tabs component

---
+++ src/components/VerticalTabs/VerticalTabs.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import VerticalTabs from './VerticalTabs';
+
+
+export default {
+    title: 'VerticalTabs',
+    component: VerticalTabs,
+  };
+  
+  export const Primary = ({...props}) => {
+  
+      return (
+        <>
+          <VerticalTabs>
+
+          </VerticalTabs>
+        </>
+      );
+    };
