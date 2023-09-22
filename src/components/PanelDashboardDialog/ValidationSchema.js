181fbf6eea943812beaf6fe85fc3672222878257
Trevor Osborne <trevor.osborne@srs.gov>
2023-09-21 20:57:29 -0600

    finished dashboard

--- src/components/PanelDashboardDialog/ValidationSchema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const yupSchema = yup.object().shape({
-  firstName: yup.string().required('Required').typeError('Required'),
-  lastName: yup.string().required('Required').typeError('Required'),
-  email: yup.string().required('Required').typeError('Required'),
-  phoneNumber: yup.string().required('Required').typeError('Required'),
-  contactMethod: yup.string().required('Required').typeError('Required'),
-});
