623b38329eb35b46954b68d637f80c5415128f02
Trevor Osborne <trevor.osborne@srs.gov>
2023-12-08 21:06:34 -0800

    added cards

---
+++ src/components/ArchivedReportsPage/ArchivedReportsPage.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ArchivedReportsPage from './ArchivedReportsPage';
+
+
+export default {
+    title: 'ArchivedReportsPage',
+    component: ArchivedReportsPage,
+  };
+
+  export const Primary = () => {
+    return (
+      <>
+        <ArchivedReportsPage />
+      </>
+    );
+  };
