261e1999f8fc06f1c9ff8aa96726d79f3f81f0d0
Trevor Osborne <trevorosborne89@yahoo.com>
2024-11-06 20:03:17 -0800

    reverted back to local settings for base sprint branch.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '',
+    '/api/NCHANDIWebsite',
     createProxyMiddleware({
-      target: 'http://ec2-54-176-45-156.us-west-1.compute.amazonaws.com:8080',
+      target: 'http://localhost:8080',
       changeOrigin: true,
     })
   );
