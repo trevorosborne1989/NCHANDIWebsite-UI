621c050bcd5033e421dccf112ad5ad8e0477cf21
Trevor Osborne <trevorosborne89@yahoo.com>
2024-11-30 21:38:58 -0800

    updated proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     createProxyMiddleware({
       // For local hosting use the below target. For sandbox, uncomment and use the second target
       // target: 'http://localhost:8080',
-      target: 'http://ec2-54-176-45-156.us-west-1.compute.amazonaws.com:8080',
+      target: 'http://ec2-54-176-52-174.us-west-1.compute.amazonaws.com:8080',
       changeOrigin: true,
     })
   );
