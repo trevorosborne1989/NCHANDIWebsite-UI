350f548a83192baa327b2d56f2269b5b5ae46346
Trevor Osborne <trevorosborne89@yahoo.com>
2024-12-11 11:04:01 -0800

    updated api files

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,6 @@ module.exports = function(app) {
   app.use(
     '/api/NCHANDIWebsite',
     createProxyMiddleware({
-      // For local hosting use the below target. For sandbox, uncomment and use the second target
-      // target: 'http://localhost:8080',
       target: 'http://ec2-13-57-33-74.us-west-1.compute.amazonaws.com:8080',
       changeOrigin: true,
     })
