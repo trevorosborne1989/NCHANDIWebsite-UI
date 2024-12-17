037b06e1979ebe404ba2b3b85419cdb3d40259dc
Trevor Osborne <trevorosborne89@yahoo.com>
2024-12-16 21:40:20 -0800

    updated snackbar message

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     onSubmit: async (values) => {
       try {
         await nchandiWebsiteService.emailContactForm({}, values);
-        enqueueSnackbar('This committee member was successfully submitted.', snackbarMessages.success.configuration);
+        enqueueSnackbar('Your request was successfully submitted.', snackbarMessages.success.configuration);
       } catch (err) {
         enqueueSnackbar('There was an error when submitting this form, please try again later or contact the Technology Chair', snackbarMessages.error.configuration);
         console.error(err);
