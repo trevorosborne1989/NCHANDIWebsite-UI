538ac6fe3a75715fb5bda61f19a073c26171d79c
Trevor Osborne <trevorosborne89@yahoo.com>
2024-12-20 15:05:06 -0800

    hot fix: fixed formik names

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -556,15 +556,15 @@ const Resources = () => {
                   <Grid sm={10} pb={5}>
                     <TextField
                       label='Phone Number'
-                      name='phoneNumber'
+                      name='phone'
                       fullWidth
                       variant='outlined'
                       margin='dense'
-                      value={formik.values.phoneNumber}
+                      value={formik.values.phone}
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
-                      helperText={formik.touched.phoneNumber ? formik.errors.phoneNumber : ""}
-                      error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+                      helperText={formik.touched.phone ? formik.errors.phone : ""}
+                      error={formik.touched.phone && Boolean(formik.errors.phone)}
                       required
                     />
                   </Grid>
