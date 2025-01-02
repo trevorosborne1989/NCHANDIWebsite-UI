092d5b7ae03190abe380c0d856b1f0948158d122
Trevor Osborne <trevorosborne89@yahoo.com>
2025-01-01 18:27:27 -0800

    set urls to localhost testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     '/api/NCHANDIWebsite',
     createProxyMiddleware({
       // For local hosting use the below target. For sandbox, uncomment and use the second target
-      // target: 'http://localhost:8080',
-      target: 'http://ec2-13-57-33-74.us-west-1.compute.amazonaws.com:8080',
+      target: 'http://localhost:8080',
+      // target: 'http://ec2-13-57-33-74.us-west-1.compute.amazonaws.com:8080',
       changeOrigin: true,
     })
   );
