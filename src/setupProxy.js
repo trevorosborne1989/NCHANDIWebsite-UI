048d43ee26cff578f8d127a4ee736b6af83271c3
Trevor Osborne <trevorosborne89@yahoo.com>
2025-04-17 12:58:34 -0700

    took out extra path context items

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api/NCHANDIWebsite',
     createProxyMiddleware({
       // For local hosting use the below target. For production, uncomment and use the second target
       // target: 'http://localhost:8080',
-      target: 'http://ec2-13-57-33-74.us-west-1.compute.amazonaws.com:8080',
+      target: 'https://ec2-13-57-33-74.us-west-1.compute.amazonaws.com:8080',
       changeOrigin: true,
     })
   );
