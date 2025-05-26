e3b3ce63662707a88b89d7c31e33325dc7d0db35
Trevor Osborne <trevorosborne89@yahoo.com>
2025-05-25 20:19:21 -0700

    updates http headers to multi part

--- src/lib/NCHANDIWebsiteService.js
+++ src/lib/NCHANDIWebsiteService.js
@@ -15,8 +15,8 @@ class NCHANDIWebsiteService {
         timeout: params.timeout ? params.timeout : 10000,
         headers: params.headers ? params.headers : {
           'Access-Control-Allow-Origin': serviceURL,
-          'Content-Type': 'application/json, application/pdf, multipart/form-data, application/octet-stream',
-          'Accept': 'application/json, application/pdf, multipart/form-data, application/octet-stream',
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'multipart/form-data',
           'Access-Control-Request-Headers': 'Content-Type, Authorization'
         }
       });
