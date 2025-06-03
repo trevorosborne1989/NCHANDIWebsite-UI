2fbdca43a94cc94237dccac74bd804d63520885d
Trevor Osborne <trevorosborne89@yahoo.com>
2025-06-02 18:26:34 -0700

    updated http request headers

--- src/lib/NCHANDIWebsiteService.js
+++ src/lib/NCHANDIWebsiteService.js
@@ -15,8 +15,8 @@ class NCHANDIWebsiteService {
         timeout: params.timeout ? params.timeout : 10000,
         headers: params.headers ? params.headers : {
           'Access-Control-Allow-Origin': serviceURL,
-          'Content-Type': 'multipart/form-data',
-          'Accept': 'multipart/form-data',
+          'Content-Type': 'multipart/form-data, image/png, image/jpeg, image/gif, application/octet-stream, application/pdf',
+          'Accept': 'multipart/form-data, image/png, image/jpeg, image/gif, application/octet-stream, application/pdf',
           'Access-Control-Request-Headers': 'Content-Type, Authorization'
         }
       });
